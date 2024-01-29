99edfe7d8fdd093f845e92d22c3cefbcbb113a8f
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2024-01-29 12:28:08 -0300

    Ok

--- scripts/expansion_inicial.gd
+++ scripts/expansion_inicial.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 #1. Zona de funciones Nodo.
-func _ready():
+func _ready() -> void:
 	pass
 
-func _process(delta):
+func _process(delta) -> void:
 	pass
 
-func _on_animation_player_animation_finished(nombre_animacion):
+func _on_animation_player_animation_finished(nombre_animacion) -> void:
 	queue_free()
